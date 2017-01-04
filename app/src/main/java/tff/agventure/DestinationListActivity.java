67cb03d1dc75bd25e8e6687b40907883d8f6722f
Jackie Robinson <jackierobinsonm@gmail.com>
2017-01-04 01:39:35 -0600

    add navigation drawer

--- app/src/main/java/tff/agventure/DestinationListActivity.java
+++ app/src/main/java/tff/agventure/DestinationListActivity.java
@@ -1,8 +1,28 @@
 package tff.agventure;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DestinationListActivity extends SingleFragmentActivity{
+
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private DrawerLayout mDrawerLayout;
+    String[] mItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        mDrawerList = (ListView) findViewById(R.id.drawer_list);
+        mItems = getResources().getStringArray(R.array.drawer_items);
+        mDrawerList.setAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, mItems));
+    }
 
     @Override
     protected Fragment createFragment() {
--- app/src/main/java/tff/agventure/SingleFragmentActivity.java
+++ app/src/main/java/tff/agventure/SingleFragmentActivity.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public abstract class SingleFragmentActivity extends FragmentActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
